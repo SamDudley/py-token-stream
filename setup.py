d56dfb522555e40155364480ee985bee06b5ba48
Sam Dudley <dudley.co.uk@gmail.com>
2017-05-04 22:50:40 +0100

    Add and update meta files
    Signed-off-by: Sam Dudley <dudley.co.uk@gmail.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name='py_token_stream',
-    version='0.0.1',
+    name='py-token-stream',
+    version='0.1.1',
     description='A friendlier API for working with tokens',
     url='https://github.com/SamDudley/py-token-stream',
     author='Sam Dudley',
     author_email='dudley.co.uk@gmail.com',
     license='MIT',
-    packages=['py_token_stream'],
-    zip_safe=False
+    packages=find_packages()
 )
---
+++ update_checklist.md
@@ -0,0 +1,9 @@
+1. run tests
+2. update the version in setup.py
+3. git
+    1. stage changed
+    2. write commit message
+    3. sign off
+    4. commit
+    5. push
+4. deploy to pypi
